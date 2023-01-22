1177ae4b55d8e4b35335049a4234dc9d51b46c62
ssujinKim <105957412+ssujinKim@users.noreply.github.com>
2023-01-22 23:39:50 +0900

    [level 0] Title: 피자 나눠 먹기 (2), Time: 0.03 ms, Memory: 77.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120815. 피자 나눠 먹기 （2）/피자 나눠 먹기 （2）.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        int pizza = 6;
+        
+        while(pizza % n != 0) {
+            pizza += 6;
+        }
+        answer = pizza / 6;
+        
+        return answer;
+    }
+}
