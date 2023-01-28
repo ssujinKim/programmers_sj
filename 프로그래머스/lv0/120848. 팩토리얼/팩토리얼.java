f8b959deaff3f05bd25099a5039773701272676e
ssujinKim <105957412+ssujinKim@users.noreply.github.com>
2023-01-28 21:32:18 +0900

    [level 0] Title: 팩토리얼, Time: 0.03 ms, Memory: 78.1 MB -BaekjoonHub

--- 프로그래머스/lv0/120848. 팩토리얼/팩토리얼.java
+++ 프로그래머스/lv0/120848. 팩토리얼/팩토리얼.java
@@ -1,3 +1,32 @@
+class Solution {
+    public int solution(int n) {
+        int answer = 0, i = 1;
+        
+        while(true) {
+            if(fact(i) == n) {
+                answer = i;
+                break;
+            }
+            else if(fact(i) > n) {
+                answer = i-1;
+                break;
+            }
+            else {
+                i++;
+                continue;
+            }
+        }
+        
+        return answer;
+    }
+    
+    public int fact(int n) {
+        if(n == 1) return 1;
+        return fact(n-1) * n;
+    }
+}
+
+/*
 import java.math.BigInteger;
 
 class Solution {
@@ -22,10 +51,10 @@ class Solution {
         
         return answer;
     }
-    
     public BigInteger factorial(int i) {
         if(i <= 1) return BigInteger.valueOf(1);
         BigInteger big_i = BigInteger.valueOf(i);
         return factorial(i-1).multiply(big_i);
     }
 }
+*/
