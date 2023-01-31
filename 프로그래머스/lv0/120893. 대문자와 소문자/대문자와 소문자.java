9d27748584e55ff92ef49eb4a36430c70eae6968
ssujinKim <105957412+ssujinKim@users.noreply.github.com>
2023-01-31 14:53:34 +0900

    [level 0] Title: 대문자와 소문자, Time: 11.14 ms, Memory: 82.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120893. 대문자와 소문자/대문자와 소문자.java
+++ 프로그래머스/lv0/120893. 대문자와 소문자/대문자와 소문자.java
@@ -5,10 +5,12 @@ class Solution {
         for(int i = 0; i < my_string.length(); i++) {
             char c = my_string.charAt(i);
             if(c >= 'A' && c <= 'Z')
-                answer += Character.toLowerCase(c);
+                answer += (char)(c+32);
+                // answer += Character.toLowerCase(c);
                 // answer += (c+"").toLowerCase();
             if(c >= 'a' && c <= 'z')
-                answer += Character.toUpperCase(c);
+                answer += (char)(c-32);
+                // answer += Character.toUpperCase(c);
                 // answer += (c+"").toUpperCase();
         }
         
