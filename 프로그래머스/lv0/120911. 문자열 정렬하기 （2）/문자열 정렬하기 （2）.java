cb0a241117f066c17434bb694fc4cc3e0f8d24e5
ssujinKim <105957412+ssujinKim@users.noreply.github.com>
2023-02-13 22:12:45 +0900

    [level 0] Title: 문자열 정렬하기 (2), Time: 2.34 ms, Memory: 73.9 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120911. 문자열 정렬하기 （2）/문자열 정렬하기 （2）.java
@@ -0,0 +1,16 @@
+import java.util.Arrays;
+
+class Solution {
+    public String solution(String my_string) {
+        String answer = "";
+        answer = my_string.toLowerCase();
+        
+        String[] arr = answer.split("");
+        Arrays.sort(arr);
+        
+        answer = "";
+        for(int i = 0; i < arr.length; i++)
+            answer += arr[i];
+        return answer;
+    }
+}
