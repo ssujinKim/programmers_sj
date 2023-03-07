e252aabbd59019e6d51b57e6698a96e9f527f548
ssujinKim <105957412+ssujinKim@users.noreply.github.com>
2023-03-08 00:03:06 +0900

    [level 0] Title: 저주의 숫자 3, Time: 0.10 ms, Memory: 76 MB -BaekjoonHub

--- 프로그래머스/lv0/120871. 저주의 숫자 3/저주의 숫자 3.java
+++ 프로그래머스/lv0/120871. 저주의 숫자 3/저주의 숫자 3.java
@@ -1,29 +1,36 @@
 class Solution {
     public int solution(int n) {
         int answer = 0;
-        int cnt = 1;
-        boolean check;
         
-        while(true) {
-            check = false;
+//         int cnt = 1;
+//         boolean check;
+        
+//         while(true) {
+//             check = false;
             
-            if(answer%3 == 0) {
-                answer++;
-            }
+//             if(answer%3 == 0) {
+//                 answer++;
+//             }
             
-            String s = answer+"";
-            if(s.contains("3")) {
-                answer++;
-            }
+//             String s = answer+"";
+//             if(s.contains("3")) {
+//                 answer++;
+//             }
             
-            if(answer%3 == 0 || s.contains("3")) {
-                check = true;
-                continue;
-            }
+//             if(answer%3 == 0 || s.contains("3")) {
+//                 check = true;
+//                 continue;
+//             }
             
-            if(cnt == n) break;
-            cnt++;
+//             if(cnt == n) break;
+//             cnt++;
+//             answer++;
+//         }
+        
+        for(int i = 1; i <= n; i++) {
             answer++;
+            if(answer%3 == 0 || String.valueOf(answer).contains("3"))
+                i--;
         }
         
         return answer;
