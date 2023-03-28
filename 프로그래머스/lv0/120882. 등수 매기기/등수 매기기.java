cfeb58afe78c57714c9ec85f77da0f8419c39940
ssujinKim <105957412+ssujinKim@users.noreply.github.com>
2023-03-28 22:37:16 +0900

    [level 0] Title: 등수 매기기, Time: 0.02 ms, Memory: 78 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120882. 등수 매기기/등수 매기기.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int[] solution(int[][] score) {
+        int[] answer = new int[score.length];
+        double[] avg = new double[score.length];
+        
+        for(int i = 0; i < score.length; i++) {
+            avg[i] = (double)(score[i][0]+score[i][1]) / 2;
+        }
+        
+        for(int i = 0; i < answer.length; i++) {
+            answer[i] = 1;
+            for(int j = 0; j < answer.length; j++) {
+                if(avg[i] < avg[j])
+                    answer[i]++;
+            }
+        }
+        
+        return answer;
+    }
+}
