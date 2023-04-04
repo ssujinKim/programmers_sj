bf8bca7cd783920cc6397f437a0dfec56d832968
ssujinKim <105957412+ssujinKim@users.noreply.github.com>
2023-04-04 18:38:47 +0900

    [level 0] Title: 옹알이 (1), Time: 0.04 ms, Memory: 78.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120956. 옹알이 （1）/옹알이 （1）.java
+++ 프로그래머스/lv0/120956. 옹알이 （1）/옹알이 （1）.java
@@ -1,7 +1,6 @@
 class Solution {
     public int solution(String[] babbling) {
         int answer = 0;
-        String[] arr = {"aya", "ye", "woo", "ma"};
         
         for(int i = 0; i < babbling.length; i++) {
             String b = babbling[i];
@@ -14,7 +13,7 @@ class Solution {
             }
             if(b.length() > 3) {
                 while(b.length() > 0) {
-                    //if(b.length() < 2) break;
+                    if(b.length() < 2) break;
                     
                     if(b.length() >= 2 && (b.substring(0,2).equals("ye") || b.substring(0,2).equals("ma"))) {
                         b = b.substring(2, b.length());
